b08cf9c5e616c1bcd86dbd0e6e600b107e2916d5
noamkorngut <73800528+noamkorngut@users.noreply.github.com>
2023-01-29 21:03:34 +0200

    Update run_recon_eval.sh

--- ReconFormer/run_recon_eval.sh
+++ ReconFormer/run_recon_eval.sh
@@ -1,3 +1,4 @@
 #!/bin/env bash
 # ReconFormer Evaluation
 python main_recon_test.py --phase test --model ReconFormer --challenge singlecoil --F_path 'path to fastMRI dataset' --test_dataset F --sequence PD --accelerations 8 --center-fractions 0.04 --checkpoint 'path to checkpoint'--verbose
+# python main_recon_test.py --phase test --model ReconFormer --challenge singlecoil --F_path /tcmldrive/Noam/AIsap/MRI_reconstruction --test_dataset F --sequence Dataset --accelerations 4 --center-fractions 0.04 --checkpoint /tcmldrive/Noam/AIsap/MRI_reconstruction/ReconFormer/checkpoints/F_X4_checkpoint.pth  --verbose
